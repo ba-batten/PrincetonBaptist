47266927f09e15cead6b21bd777c75340f46600d
Brandon Batten <ba_batten@live.com>
2018-03-13 23:28:23 -0400

    Add function to add a map to the contacts page

--- js/map.js
+++ js/map.js
@@ -1,34 +1,52 @@
 function modalMap() {
-        var pbc = { lat:35.4633771, lng: -78.1593011 };
-        var map = new google.maps.Map(document.getElementById('modal-map'), {
-          zoom: 15,
-          center: { lat:35.4633771, lng: -78.1593011 }
-        });
+  var pbc = { lat:35.4633771, lng: -78.1593011 };
+  var map = new google.maps.Map(document.getElementById('modal-map'), {
+    zoom: 15,
+    center: { lat:35.4633771, lng: -78.1593011 }
+  });
 
-        var marker = new google.maps.Marker({
-          position: pbc,
-          map: map
-        });
-      }
+  var marker = new google.maps.Marker({
+    position: pbc,
+    map: map
+  });
+}
 
 $('#servicesModal').on('shown.bs.modal', function () {
   modalMap();
 });
 
 function footerMap() {
-        var pbc = { lat:35.4633771, lng: -78.1593011 };
-        var map = new google.maps.Map(document.getElementById('footerMap'), {
-          zoom: 15,
-          center: { lat:35.4633771, lng: -78.1593011 }
-        });
+  var pbc = { lat:35.4633771, lng: -78.1593011 };
+  var map = new google.maps.Map(document.getElementById('footerMap'), {
+    zoom: 15,
+    center: { lat:35.4633771, lng: -78.1593011 }
+  });
 
-        var marker = new google.maps.Marker({
-          position: pbc,
-          map: map
-        });
+  var marker = new google.maps.Marker({
+    position: pbc,
+    map: map
+  });
+};
+
+function contactMap() {
+  var pbc = { lat:35.4633771, lng: -78.1593011 };
+  var map = new google.maps.Map(document.getElementById('contact-map'), {
+    zoom: 15,
+    center: { lat:35.4633771, lng: -78.1593011 }
+  });
+
+  var marker = new google.maps.Marker({
+    position: pbc,
+    map: map
+  });
 };
 
 function initMap() {
   modalMap();
   footerMap();
+}
+
+function footerAndContactMaps() {
+  footerMap();
+  contactMap();
 }
