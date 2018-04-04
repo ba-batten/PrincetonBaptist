7bbaf9109d2da9eb0f45ccd2633b247a78296f99
Brandon Batten <ba_batten@live.com>
2018-04-03 22:35:49 -0400

    Correct grammatical errors for AWANA

--- js/model.js
+++ js/model.js
@@ -180,25 +180,25 @@ function AppViewModel() {
       group: 'AWANA',
       pic: 'images/awana-clubs-logo-color.svg',
       description: 'AWANA is a scripture memory club for children that is filled ' +
-      'with fun games and Bible learning, and is a great tool for discipleship for ' +
+      'with fun games and Bible learning, and is a great discipleship tool for ' +
       'families.\n\nAWANA is an acronym that stands for Approved Workmen Are Not ' +
-      'Ashamed (taken from 2 Timothy 2:15). Your child will learn Bible verses that ' +
+      'Ashamed (taken from 2 Timothy 2:15). Children will learn Bible verses that ' +
       'will be hidden in their hearts that will help them for years to come!\n\nAWANA ' +
       'is intended to be a parent participation program. We need parents\' help in ' +
       'order to make the AWANA club great. We ask parents to work alongside their ' +
-      'children at club night and throughout the week, to ensure we have a strong ' +
+      'children during club night and throughout the week, to ensure we have a strong ' +
       'program and to maximize discipleship.'
     },
     {
       group: 'Puggles',
       pic: 'images/puggles.svg',
-      description: 'Puggles is an enriching program for your pre-Cubbies kids ' +
+      description: 'Puggles is an enriching program for pre-Cubbies kids ' +
       '(usually older 2- and younger 3-year-olds) that presents basic biblical' +
       ' concepts:\n' + '•   We are a unique creation of God.\n•   God loves us very much.\n' +
       'Puggles teaches toddlers simple lessons through teaching cards, activities, ' +
       'music and more. Then parents—an Awana Puggle’s first and best teacher—can ' +
       'step in to reinforce the lessons with take-home cards that include ideas ' +
-      'to point out God’s work each day. And little details like Awana Puggles ' +
+      'to point out God’s work each day. Little details like Awana Puggles ' +
       'T-shirts, puzzles and coloring books make every precious child feel like ' +
       'part of the group.'
     },
@@ -208,7 +208,7 @@ function AppViewModel() {
       description: 'Cubbies (four year olds by September 1) - Fun... and lots of ' +
       'it! Awana Cubbies is fun with a purpose; to reach and train kids for Jesus ' +
       'Christ. Games, puppets, awards and other activities transform our preschool ' +
-      'program into a lively weekly experience where fun and learning go hand in hand.'
+      'program into a lively experience where fun and learning go hand in hand.'
     },
     {
       group: 'Sparks',
@@ -226,7 +226,7 @@ function AppViewModel() {
     {
       group: 'Truth & Training',
       pic: 'images/TT_logo_large.svg',
-      description: 'Truth and Training (third - sixth grade) - Exciting features ' +
+      description: 'Truth & Training (third - sixth grade) - Exciting features ' +
       'to get kids excited about God\'s Word. Truth & Training is equipped with a ' +
       'variety of dynamic tools to accomplish this mission:\n' +
       '•    Four handbooks of increasing difficulty which teach children that life\'s '+
